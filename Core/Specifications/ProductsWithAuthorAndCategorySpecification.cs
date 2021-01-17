e232527a95bddebb2ed3db41f9b587dc0f3652e5
Diana <diana.incr@yahoo.com>
2021-01-17 16:39:01 +0200

    Filter, Sort, Search

--- Core/Specifications/ProductsWithAuthorAndCategorySpecification.cs
+++ Core/Specifications/ProductsWithAuthorAndCategorySpecification.cs
@@ -6,10 +6,35 @@ namespace Core.Specifications
 {
     public class ProductsWithAuthorAndCategorySpecification : BaseSpecification<Product>
     {
-        public ProductsWithAuthorAndCategorySpecification()
+        public ProductsWithAuthorAndCategorySpecification(ProductSpecParams productParams)
+            : base(x =>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower().Contains
+                (productParams.Search)) &&
+                (!productParams.CategoryId.HasValue || x.CategoryId == productParams.CategoryId) &&
+                (!productParams.AuthorId.HasValue || x.AuthorId == productParams.AuthorId)
+            )
         {
             AddInclude(x => x.Author);
             AddInclude(x => x.Category);
+            AddOrderBy(x => x.Name);
+            ApplyPaging(productParams.PageSize * (productParams.PageIndex - 1),
+            productParams.PageSize);
+
+            if(!string.IsNullOrEmpty(productParams.Sort))
+            {
+                switch(productParams.Sort)
+                {
+                    case "priceAsc":
+                        AddOrderBy(p => p.Price);
+                        break;
+                    case "priceDesc":
+                        AddOrderByDescending(p => p.Price);
+                        break;
+                    default:
+                        AddOrderBy(n => n.Name);
+                        break;
+                }
+            }
         }
 
         public ProductsWithAuthorAndCategorySpecification(int id) : base(x => x.Id == id)
--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -34,6 +34,7 @@ namespace Infrastructure.Data
 
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
+
             return await _context.Products
             .Include(p => p.Author)
             .Include(p => p.Category)
