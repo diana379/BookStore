ba67eaaa4af6b43cb86561b957e412f35d397f8e
Diana <diana.incr@yahoo.com>
2021-01-22 00:06:01 +0200

    Basket UI

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent,data:{breadcrumb:'Home'}},
+  {path: '', component: HomeComponent, data: {breadcrumb: 'Home'}},
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule),
-  data:{breadcrumb:'Shop'}},
+  data: {breadcrumb: 'Shop'}},
+  {path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule),
+  data: {breadcrumb: 'Basket'}},
+  {path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule),
+  data: {breadcrumb: 'Checkout'}},
   {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
 
